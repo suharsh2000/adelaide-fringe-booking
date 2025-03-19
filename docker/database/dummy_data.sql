dfac87e3ddacd79e36c15be4e91fee2a70558d56
SUHAR <shar1217@flinders.edu.au>
2025-03-19 23:28:40 +1030

    Added Dockerfile and docker-compose files for frontend and backend

---
+++ docker/database/dummy_data.sql
@@ -0,0 +1,6 @@
+-- Insert some dummy users
+INSERT INTO users (email, password) VALUES
+('test@example.com', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi'), -- password is 'password'
+('admin@example.com', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi');
+
+-- Add other dummy data as needed
